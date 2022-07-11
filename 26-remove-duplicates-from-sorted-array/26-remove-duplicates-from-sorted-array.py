2ff0df74bfaf72d801f9340b0e3273d8f704d9de
Aman Rai <87781556+HEYRAIAMAN@users.noreply.github.com>
2022-07-12 03:08:40 +0530

    Time: 113 ms (75.31%), Space: 15.5 MB (60.64%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -4,5 +4,6 @@ class Solution:
         for i in range(1, len(nums)):
             if nums[i] != nums[i - 1]:
                 nums[l] = nums[i]
-                l += 1
+                l += 1 
+              
         return l 
