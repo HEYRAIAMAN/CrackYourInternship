397b844596464c64d4e99474591f73c278e17be1
Aman Rai <87781556+HEYRAIAMAN@users.noreply.github.com>
2022-07-12 03:10:43 +0530

    Time: 21 ms (42.55%), Space: 18.3 MB (74.81%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -4,8 +4,7 @@ public:
         int j = 1;
         for(int i = 1; i< nums.size(); i++){
             if(nums[i] != nums[i-1]){
-                nums[j]=nums[i];
-                j+=1;
+                nums[j++]=nums[i];
                 
             }
           
