1c9292bd2995b66991fd90ffc9b3b6183da5a2b5
Aman Rai <87781556+HEYRAIAMAN@users.noreply.github.com>
2022-07-14 23:40:14 +0530

    Time: 1121 ms (11.87%), Space: 15.6 MB (65.09%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,7 +4,4 @@ class Solution:
             nums.remove(0)
             nums.append(0)
         return nums
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         
